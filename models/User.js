4c82dc536c9029f5fc5ab7b8e88818fdc633e225
Eric Scott <scottzillaster@gmail.com>
2018-08-23 09:27:36 -0500

    finished

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
+
+const UserSchema = new Schema({
+  username: { type: String, required: true, unique: true},
+  password: { type: String, required: true }
+});
+
+UserSchema.pre('save', function(next){
+  if(this.isModified('password') || this.isNew){
+    // hash the password
+    bcrypt.hash(this.password, 10, (err, hash) => {
+      if(err){ return next(err); }
+      this.password = hash;
+      return next();
+    });
+  } else {
+    return next();
+  }
+});
+
+UserSchema.methods.comparePassword = function(pass, cb){
+  bcrypt.compare(pass, this.password, (err, isMatch) => {
+    if(err){ return cb(err); }
+    cb(null, isMatch);
+  });
+}
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
--- server.js
+++ server.js
@@ -3,8 +3,33 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const path = require("path");
 const Note = require("./models/Note");
+const User = require("./models/User");
+const dotenv = require("dotenv");
 
+const passport = require("passport");
+const { Strategy:JwtStrategy, ExtractJwt } = require("passport-jwt");
+dotenv.config();
+
+const jwt = require("jsonwebtoken");
 const PORT = process.env.PORT || 3001;
+
+const passportOpts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET
+};
+
+passport.use(new JwtStrategy(passportOpts,
+(jwt_payload, done) => {
+  User.findOne({_id: jwt_payload._id}, (err, user) => {
+    if(err) {return done(err, false);}
+    if(user){
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  })
+}));
+
 
 const app = express();
 
@@ -20,8 +45,49 @@ if (process.env.NODE_ENV === "production") {
 app.get("/api/notes", function(req, res){
   Note.find({}).then(dbModel => res.json(dbModel));
 })
-app.post("/api/create", function(req, res){
+app.post("/api/create",  function(req, res){
   Note.create(req.body).then(dbModel => res.json(dbModel));
+})
+
+app.get("/api/test", passport.authenticate('jwt', {session: false}), (req, res) => {
+  res.json({accessible: true});
+});
+
+app.post("/api/auth/register", (req, res) => {
+  console.log(req.body);
+  var user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+  user.save(err => {
+    if(err){
+      return res.json({success: false, message: "Username taken"});
+    }
+    return res.json({success: true, message: "Successfully created new user"});
+  })
+});
+
+app.post("/api/auth/login", (req, res) => {
+  User.findOne({
+    username: req.body.username
+  }, (err, user) => {
+    if(!user){
+      res.status(401).send({success: false, message: "wrong username"});
+    } else {
+      user.comparePassword(req.body.password, (err, isMatch) => {
+          if(!err && isMatch){
+            const token = jwt.sign({
+              _id: user._id,
+              username: user.username
+            }, process.env.JWT_SECRET);
+
+            res.json({success: true, token: "JWT " + token});
+          } else {
+            res.status(401).send({success: false, message: "wrong username or password"});
+          }
+      });
+    }
+  });
 })
 
 app.use((req, res) => {
